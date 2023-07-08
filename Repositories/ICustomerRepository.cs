25e3d29b757b5c809e0e5c89818c4db1faeb48ac
eskokado <eskokado@gmail.com>
2023-07-08 15:55:35 -0300

    feat: add methods FindById, Update, ListPerPage in interface CustomerRepository

--- Repositories/ICustomerRepository.cs
+++ Repositories/ICustomerRepository.cs
@@ -3,6 +3,8 @@ namespace appesk.Repositorties
   public interface ICustomerRepository
   {
     CustomerModel Create(CustomerModel customer);
-    List<CustomerModel> GetCustomersFromDatabase(int pageNumber, int pageSize);
+    CustomerModel Update(CustomerModel customer);
+    List<CustomerModel> ListPerPage(int pageNumber, int pageSize);
+    CustomerModel? FindById(int id);
   }
 }
