fe54f6e12093e02b207bb54ffc57e1ae3f375616
eskokado <eskokado@gmail.com>
2023-07-09 08:19:06 -0300

    feat: pagination using package PageList in ICustomerRepository

--- Repositories/ICustomerRepository.cs
+++ Repositories/ICustomerRepository.cs
@@ -1,10 +1,12 @@
+using X.PagedList;
+
 namespace appesk.Repositorties
 {
   public interface ICustomerRepository
   {
     CustomerModel Create(CustomerModel customer);
     CustomerModel Update(CustomerModel customer);
-    List<CustomerModel> ListPerPage(int pageNumber, int pageSize);
+    IPagedList<CustomerModel> ListPerPage(int pageNumber, int pageSize);
     CustomerModel? FindById(int id);
 
     bool DeleteById(int id);
