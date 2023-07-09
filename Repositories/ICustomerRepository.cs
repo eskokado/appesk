6cb4ffd7146ff47384f49b37e11381654f66f90a
eskokado <eskokado@gmail.com>
2023-07-09 11:11:04 -0300

    feat: add GetFiltered in interface ICustomerRepository

--- Repositories/ICustomerRepository.cs
+++ Repositories/ICustomerRepository.cs
@@ -2,13 +2,14 @@ using X.PagedList;
 
 namespace appesk.Repositorties
 {
-  public interface ICustomerRepository
-  {
-    CustomerModel Create(CustomerModel customer);
-    CustomerModel Update(CustomerModel customer);
-    IPagedList<CustomerModel> ListPerPage(int pageNumber, int pageSize);
-    CustomerModel? FindById(int id);
+	public interface ICustomerRepository
+	{
+		CustomerModel Create(CustomerModel customer);
+		CustomerModel Update(CustomerModel customer);
+		IPagedList<CustomerModel> ListPerPage(int pageNumber, int pageSize);
+		IPagedList<CustomerModel> GetFiltered(int pageNumber, int pageSize, string? name, string? email, string? phone, DateTime? registrationDate, bool? isBlocked);
+		CustomerModel? FindById(int id);
 
-    bool DeleteById(int id);
-  }
+		bool DeleteById(int id);
+	}
 }
