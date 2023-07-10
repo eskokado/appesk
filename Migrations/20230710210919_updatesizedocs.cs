2b481fb00ed6a3218001f1a0d7be1fe19b305a35
eskokado <eskokado@gmail.com>
2023-07-10 18:49:56 -0300

    chore: migration size docs

---
+++ Migrations/20230710210919_updatesizedocs.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace appesk.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatesizedocs : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "StateRegistration",
+                table: "Customers",
+                type: "varchar(15)",
+                maxLength: 15,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(12)",
+                oldMaxLength: 12)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CPF_CNPJ",
+                table: "Customers",
+                type: "varchar(19)",
+                maxLength: 19,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(14)",
+                oldMaxLength: 14)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "StateRegistration",
+                table: "Customers",
+                type: "varchar(12)",
+                maxLength: 12,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(15)",
+                oldMaxLength: 15)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CPF_CNPJ",
+                table: "Customers",
+                type: "varchar(14)",
+                maxLength: 14,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(19)",
+                oldMaxLength: 19)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
