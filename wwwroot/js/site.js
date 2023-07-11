73963a619e4a8c7905c8b484da6b7a3a049289e3
eskokado <eskokado@gmail.com>
2023-07-11 09:19:30 -0300

    feat: create scripts main

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,51 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function validateField(form, fieldSelector, urlHandler, messageError) {
+  var field = form.find(fieldSelector);
+  var data = field.val();
+  var id = form.find("#Id").val();
 
-// Write your JavaScript code.
+  if (data == "") return false;
+  
+  var isValid = false;
+
+  var ajaxRequest = $.ajax({
+      url: urlHandler,
+      type: 'POST',
+      async: false,
+      data: { data: data, id: id }
+  });
+
+  ajaxRequest.done(function(response) {
+      isValid = true;
+  });
+
+  ajaxRequest.fail(function(error) {
+      sendMessage(messageError);
+  });
+
+  return isValid;
+}
+
+function sendMessage(message, type) {
+  Toastify({
+      text: message,
+      duration: 3000,
+      gravity: "top",
+      position: "top",
+      close: true,
+      backgroundColor: type ? type : "#ff8800",
+      stopOnFocus: true,
+      offset: {
+          y: 100,
+          x: 20,
+      }
+  }).showToast();
+}
+
+function back() {
+  console.log("back");
+  window.location.href = '/Customer/Index';
+}
+
+function removeMask(value) {
+  return value.replace(/[^\d]+/g, '');
+}
